6f7f8dadbc6be50ee0c0a79eea851179ae8dbdbc
Thiago Costa <thiagonsc.costa@hotmail.com>
2022-02-18 00:21:15 -0300

    more tests

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,3 +1,4 @@
+import { InternalError } from '@src/util/errors/inernal-error';
 import { AxiosStatic } from 'axios';
 
 export interface StormGlassPointSource {
@@ -30,6 +31,20 @@ export interface ForecastPoint {
   windSpeed: number;
 }
 
+export class ClientRequestError extends InternalError {
+    constructor(message: string) {
+        const internalMessage = 'Unexpected error when trying to communicate to StormGlass';
+        super(`${internalMessage}: ${message}`);
+    }
+}
+
+export class StormGlassResponseError extends InternalError {
+    constructor(message: string) {
+        const internalMessage = 'Unexpected error when returned by the StormGlass service';
+        super(`${internalMessage}: ${message}`);
+    }
+}
+
 export class StormGlass {
   readonly params =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection';
@@ -39,10 +54,18 @@ export class StormGlass {
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lgn: number): Promise<ForecastPoint[]> {
-    const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lgn}&params=${this.params}`
-    );
-    return this.normalizeResponse(response.data);
+      try {
+          const response = await this.request.get<StormGlassForecastResponse>(
+            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lgn}&params=${this.params}`
+          );
+          return this.normalizeResponse(response.data);
+
+      } catch (err: any) {
+          if (err.response && err.response.status) {
+            throw new StormGlassResponseError(`Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`)
+          }
+        throw new ClientRequestError(err.message);
+      }
   }
 
   private normalizeResponse(
